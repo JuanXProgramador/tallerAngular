c1c55793d29f5dbfc43653d53768206d34e66ce6
JuanXProgramador <juan4cardenas@gmail.com>
2024-05-04 14:54:29 -0500

    ahora si

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -7,20 +7,42 @@ import { SerieService } from './serie.service';
   templateUrl: './serie.component.html',
   styleUrls: ['./serie.component.css']
 })
+
+
+
 export class SerieComponent implements OnInit {
 
   series: Array<Serie> = [];
+
+  promedio:number = 0; 
+
+  seriesTable: HTMLElement = document.getElementById("series")!;
 
   constructor(private serieService: SerieService) { }
 
   getSeries(): void {
     this.serieService.getSeries().subscribe((series) => {
       this.series = series;
+      this.promedio = this.promedioSeasons(series);
     });
   }
 
   ngOnInit(): void {
     this.getSeries();
   }
+
+  ngAfterViewInit(): void{
+    this.promedioSeasons(this.series);
+  }
+
+  promedioSeasons(series:Serie[]):number{
+    let promedio:number = 0;
+    let n:number = 0;
+    for(let serie of series){
+        promedio += serie.seasons;
+        n++;
+    }
+    return (promedio/n);
+  }  
 
 }
